d540b1fc8cc5096488c580d9a7b36da13af43c3b
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-02-15 22:32:08 +0530

    Food Pin - 2 Exercise

---
+++ FoodPin-2/FoodPin-2/RestaurantTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin-2
+//
+//  Created by Prakhar Kumar on 15/02/21.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBOutlet var nameLabel: UILabel!;
+    @IBOutlet var typeLabel: UILabel!;
+    @IBOutlet var locationLabel: UILabel!;
+    @IBOutlet var thumbnailImage: UIImageView! {
+        didSet {
+            thumbnailImage.layer.cornerRadius = 20.0;
+            thumbnailImage.clipsToBounds = true;
+        }
+    };
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodPin-2/FoodPin-2/RestaurantViewTableController.swift
@@ -0,0 +1,65 @@
+//
+//  RestaurantViewTableController.swift
+//  FoodPin-2
+//
+//  Created by Prakhar Kumar on 15/02/21.
+//
+
+import UIKit
+
+class RestaurantViewTableController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = dataSource;
+        
+        tableView.separatorStyle = .none;
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>();
+        
+        snapshot.appendSections([.all]);
+        snapshot.appendItems(restaurantNames, toSection: .all)
+        
+        dataSource.apply(snapshot, animatingDifferences: false);
+    }
+    
+    enum Section {
+        case all;
+    };
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"];
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"];
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
+        
+        let cellIdentifier = "datacell";
+        let dataSource = UITableViewDiffableDataSource<Section, String>(
+            tableView: tableView,
+            cellProvider: {
+                tableView,
+                indexPath,
+                restaurantName in
+                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell;
+                
+                cell.nameLabel.text = restaurantName;
+                cell.typeLabel.text = self.restaurantTypes[indexPath.row];
+                cell.locationLabel.text = self.restaurantLocations[indexPath.row];
+                cell.thumbnailImage.image = UIImage(named: self.restaurantImages[indexPath.row]);
+                
+                
+                return cell;
+            }
+        );
+        
+        return dataSource;
+    }
+    
+    lazy var dataSource = configureDataSource();
+
+}
