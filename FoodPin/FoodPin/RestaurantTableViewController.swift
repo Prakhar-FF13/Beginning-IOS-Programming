6ea3e365b789df5a308aa2021e0ab2f9cee1900f
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-02-15 21:40:36 +0530

    Food Pin - 1 Complete

--- FoodPin/FoodPin/RestaurantTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -26,6 +26,10 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"];
     
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
     enum Section {
         case all;
     };
@@ -42,6 +46,8 @@ class RestaurantTableViewController: UITableViewController {
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
                 cell.nameLabel.text = restaurantName
                 cell.thumbnailImageView.image = UIImage(named:self.restaurantImages[indexPath.row])
+                cell.locationLabel.text = self.restaurantLocations[indexPath.row];
+                cell.typeLabel.text = self.restaurantTypes[indexPath.row];
                 
                 return cell
             }
