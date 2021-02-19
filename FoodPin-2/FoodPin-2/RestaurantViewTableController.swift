83d60d96a54cb88161fb7e8bbda5d82480d2b587
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-02-19 20:17:52 +0530

    Alert controller for ipad and iphone to mark a restaurant as favourite

--- FoodPin-2/FoodPin-2/RestaurantViewTableController.swift
+++ FoodPin-2/FoodPin-2/RestaurantViewTableController.swift
@@ -11,6 +11,7 @@ class RestaurantViewTableController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.cellLayoutMarginsFollowReadableWidth = true;
         
         tableView.dataSource = dataSource;
         
@@ -36,6 +37,8 @@ class RestaurantViewTableController: UITableViewController {
 
     var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
+    var restaurantIsFavourite = Array(repeating: false, count: 21);
+    
     func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
         
         let cellIdentifier = "datacell";
@@ -51,6 +54,7 @@ class RestaurantViewTableController: UITableViewController {
                 cell.typeLabel.text = self.restaurantTypes[indexPath.row];
                 cell.locationLabel.text = self.restaurantLocations[indexPath.row];
                 cell.thumbnailImage.image = UIImage(named: self.restaurantImages[indexPath.row]);
+                cell.accessoryType = self.restaurantIsFavourite[indexPath.row] ? .checkmark : .none;
                 
                 
                 return cell;
@@ -65,10 +69,19 @@ class RestaurantViewTableController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet);
         
+        if let popoverController = optionMenu.popoverPresentationController {
+            if let cell = tableView.cellForRow(at: indexPath) {
+                popoverController.sourceView = cell;
+                popoverController.sourceRect = cell.bounds;
+            }
+        }
+        
+        // cancel action.
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil);
         
         optionMenu.addAction(cancelAction);
         
+        // reserve action.
         let reserveActionHandler = { (action: UIAlertAction!) -> Void in
             let alertMessage = UIAlertController(title: "Not available yet", message: "Sorry, this feature is not available yet. Please retry later.", preferredStyle: .alert);
             
@@ -81,13 +94,15 @@ class RestaurantViewTableController: UITableViewController {
         
         optionMenu.addAction(reserveAction);
         
-        
+        // favourite action.
         let favouriteAction = UIAlertAction(title: "Favourite", style: .default, handler: {
             (action: UIAlertAction!) -> Void in
             
             let cell = tableView.cellForRow(at: indexPath)
             
             cell?.accessoryType = .checkmark;
+            cell?.tintColor = .systemYellow;
+            self.restaurantIsFavourite[indexPath.row] = true;
         });
             
         optionMenu.addAction(favouriteAction);
